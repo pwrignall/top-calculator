ec8898afad8743df4817989ce6f6d083eb30b379
Paul Rignall <paul.rignall@gmail.com>
2020-06-18 16:57:39 +0100

    Basic functions, structure, style

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function add(m, n) {
+    return m + n;
+}
+
+function subtract(m, n) {
+    return m - n;
+}
+
+function multiply(m, n) {
+    return m * n;
+}
+
+function divide(m, n) {
+    return m / n;
+}
+
+function operate(operation, m, n) {
+    return operation(m, n);
+}
